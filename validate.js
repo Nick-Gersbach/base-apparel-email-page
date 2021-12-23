276d7d609af76aa43b639b338b97ab58c03dac49
Nicholas Gersbach <nicholas.j.gersbach@gmail.com>
2021-12-23 07:21:21 -0700

    email input fix

--- validate.js
+++ validate.js
@@ -10,7 +10,16 @@ function submitEmail(e) {
   if (emailInput.value === "") {
     smalltext.style.visibility = "visible";
     errorImg.style.visibility = "visible";
+  } else if (isValidEmail(!emailInput.value)) {
+    smalltext.style.visibility = "visible";
+    errorImg.style.visibility = "visible";
   }
   e.preventDefault();
   console.log("form submitted");
+}
+
+function isValidEmail() {
+  const filter =
+    /^\s*[\w\-\+_]+(\.[\w\-\+_]+)*\@[\w\-\+_]+\.[\w\-\+_]+(\.[\w\-\+_]+)*\s*$/;
+  return String(e).search(filter) != -1;
 }
