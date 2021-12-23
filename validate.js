b6e43fc4d3bbd439dfb7ceff89cd4e188334c630
Nicholas Gersbach <nicholas.j.gersbach@gmail.com>
2021-12-22 19:13:45 -0700

    js form

--- validate.js
+++ validate.js
@@ -1,1 +1,16 @@
+const smalltext = document.getElementById("small-text");
+const emailForm = document.getElementById("form");
+const emailSubmit = document.getElementById("email-btn");
+const emailInput = document.getElementById("email-input");
+const errorImg = document.getElementById("error-img");
 
+emailSubmit.addEventListener("click", submitEmail);
+
+function submitEmail(e) {
+  if (emailInput.value === "") {
+    smalltext.style.visibility = "visible";
+    errorImg.style.visibility = "visible";
+  }
+  e.preventDefault();
+  console.log("form submitted");
+}
